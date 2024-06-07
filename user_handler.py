82f61eaccff70bcc5cf6030b946fe7a9be6242ef
BuildTools <unconfigured@null.spigotmc.org>
2024-06-07 22:08:25 +0200

    Pokus o implementaci refresh tokenu, nefunkcni, program bezi

--- user_handler.py
+++ user_handler.py
@@ -3,7 +3,8 @@ import json
 import token_handler
 from dataclasses import dataclass
 
-class User():
+
+class User:
     def __init__(self):
         # todo hardcoded for testing
         self.personid = "E2196244"
@@ -11,38 +12,39 @@ class User():
 
     def get_data(self):
         response = requests.get(
-            'https://aplikace.skolaonline.cz/solapi/api/v1/user',
-            headers={
-                'Authorization': f'Bearer {token_handler.get_token_from_file()}'
-            })
+            "https://aplikace.skolaonline.cz/solapi/api/v1/user",
+            headers={"Authorization": f"Bearer {token_handler.get_token_from_file()}"},
+        )
 
         if response.status_code == 401 or response.status_code == 400:
-            raise Exception('token expired')
-        # todo dalo by se udělat, když nevyjde request, aby se podíval do cache, 
+            token_handler.get_token_from_refresh_token()
+            response = requests.get(
+                "https://aplikace.skolaonline.cz/solapi/api/v1/user",
+                headers={
+                    "Authorization": f"Bearer {token_handler.get_token_from_file()}"
+                },
+            )
+            if response.status_code == 401 or response.status_code == 400:
+                raise Exception("token expired")
+        # todo dalo by se udělat, když nevyjde request, aby se podíval do cache,
         # kam by se průběžně každý request ukládal
 
         # netuším, jak tohle funguje, ale funguje to
         data = response.json()
-        data['classroom'] = data['class']
+        data["classroom"] = data["class"]
         self.ostatni = data
 
-        self.personid = data['personID']
-        self.school_year_id = data['schoolYearId']
-        self.modules = data['enabledModules']
+        self.personid = data["personID"]
+        self.school_year_id = data["schoolYearId"]
+        self.modules = data["enabledModules"]
         # todo vymyslet, jak to vlastně ukládat
-    
-
-            
 
     # todo třeba i implementovat ten zbytek, tohle zatím stačí (snad)
-
-
-
 
 
 def main():
     pass
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
