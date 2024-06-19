b60ef21678becd493be3e69c1c37f59d9b217766
Michael36500 <ambros.michael@seznam.cz>
2024-06-19 10:40:15 +0200

    e

---
+++ absences.py
@@ -0,0 +1,69 @@
+import requests
+from dataclasses import dataclass
+import json
+import token_handler
+import time, datetime
+
+def get_absences_download(user):
+    r = requests.get(
+        f'https://aplikace.skolaonline.cz/solapi/api/v1/absences/inPeriod',
+        headers={
+                'Authorization': f'Bearer {token_handler.get_token_from_file()}'
+            },
+        params={
+            'dateFrom': date_from(),
+            'dateTo': date_to(),
+        })
+
+    return r.text
+
+def date_from():
+    return formated_date('2023-09-01')
+    
+def date_to():
+    return formated_date('2024-6-30')
+
+def formated_date(date):
+    return time.strftime('%Y-%m-%dT%H:%M:%S.000', time.strptime(date, '%Y-%m-%d'))
+
+
+@dataclass
+class DayOfAbsences:
+    date: str
+    excused: int
+    unexcused: int
+    notcounted: int
+    unevaluated: int
+    unevaluated_with_apology: int
+
+
+def absences_parser(jsn):
+    jsn = json.loads(jsn)['absences']
+    absences = {}
+    for day in jsn:
+        absences[day['date']] = DayOfAbsences(
+            day['date'],
+            day['numberOfExcused'],
+            day['numberOfUnexcused'],
+            day['numberOfNotCounted'],
+            day['numberOfUnevaluated'],
+            day['numberOfUnevaluatedWithApology']
+        )
+
+
+    return absences
+
+
+
+if __name__ == '__main__':
+    pass
+    # import user_handler
+    # import requests
+    # import token_handler
+    # import json
+    # import datetime
+    # import time
+
+    # user = user_handler.User()
+    # user.get_data()
+    # absences_parser(get_absences_download(user))
