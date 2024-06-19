d3192f2c44b9fd981993c22e96416aafd59c9c54
DaalbuCZ <daalbu@post.cz>
2024-06-19 20:45:03 +0200

    Quick fix of refresh token logic breaking regular login

--- user_handler.py
+++ user_handler.py
@@ -20,7 +20,7 @@ class User:
 
         # if unauthorized or bad credentials tries to get a new token from the refresh token
         if response.status_code == 401 or response.status_code == 400:
-            token_handler.write_token_to_file()
+            token_handler.write_token_to_file_from_refresh_token()
             response = requests.get(
                 "https://aplikace.skolaonline.cz/solapi/api/v1/user",
                 headers={
